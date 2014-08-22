4dc3bc26928967a7063a92402b8a5b4ce7c8626c
Omastars <pokemonhdstudio@gmail.com>
2014-08-22 13:12:33 +0300

    C# Tutorial
    hi

---
+++ WindowsFormsApplication7/WindowsFormsApplication7/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApplication7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 255);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open ROM";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(1)))));
+            this.label1.Location = new System.Drawing.Point(9, 387);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Loaded Game:";
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.Enabled = false;
+            this.btnEnable.Location = new System.Drawing.Point(290, 255);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(179, 50);
+            this.btnEnable.TabIndex = 2;
+            this.btnEnable.Text = "Enable Running Indoor";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.Enabled = false;
+            this.btnDisable.Location = new System.Drawing.Point(290, 332);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(179, 50);
+            this.btnDisable.TabIndex = 3;
+            this.btnDisable.Text = "Disable Running Indoor";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication7.Properties.Resources.funny_wombat;
+            this.pictureBox1.Location = new System.Drawing.Point(-4, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(507, 250);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Font = new System.Drawing.Font("Segoe WP", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(475, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Wombat\'s Advanced Running Indoor Editor";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 409);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.btnEnable);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.Button btnDisable;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication7/WindowsFormsApplication7/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OpenFileDialog ofd = new OpenFileDialog();
+
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+
+            ofd.Filter = "GBA File(*.gba)|*.gba";
+            if (ofd.ShowDialog() == DialogResult.OK) 
+            this.Text ="Loaded File: "+ ofd.FileName;
+            BinaryReader br = new BinaryReader(File.OpenRead(ofd.FileName));
+            string gamecode = null;
+            for (int i = 0xAC; i <= 0xAF; i++)
+            {
+                br.BaseStream.Position = i;
+                gamecode += br.ReadByte().ToString("X2");
+
+            }
+            br.Close();
+            if (gamecode == "42505245")
+            {
+                label1.Text = "Loaded Game: Pokémon FireRed";
+                btnEnable.Enabled = true;
+                btnDisable.Enabled = true;
+            }
+            else
+            {
+                label1.Text = "Loaded Game: ???";
+                btnEnable.Enabled = false;
+                btnDisable.Enabled = false;
+                MessageBox.Show ("The loaded game isn't supported");
+            }
+        }
+
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+            BinaryWriter bw = new BinaryWriter(File.OpenWrite(ofd.FileName));
+            for (int x = 0xBD494; x <= 0xBD494; x++)
+            {
+
+                bw.BaseStream.Position = x;
+                bw.Write(0x00);
+
+
+            }
+            bw.Close();
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            BinaryWriter bw = new BinaryWriter(File.OpenWrite(ofd.FileName));
+            for (int x = 0xBD494; x <= 0xBD494; x++)
+            {
+
+                bw.BaseStream.Position = x;
+                bw.Write(0x08);
+
+
+            }
+            bw.Close();
+        }
+    }
+}
